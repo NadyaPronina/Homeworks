7b2cdc9df65c502ca1d07933ed2bd1b612f811ea
NadyaPronina <113007223+NadyaPronina@users.noreply.github.com>
2022-09-28 14:19:47 +0300

    8kye.cpp

--- CODEWARS/8kye.cpp
+++ CODEWARS/8kye.cpp
@@ -107,3 +107,74 @@ int twice_as_old(int dad, int son) {    //twice as old
   else 
     return dad - 2*son;
 }
+
+
+#include <string>           //number to string
+
+std::string number_to_string(int num) {
+  std::string str = "";
+  std::string str1 = "";
+  int check;
+  if (num < 0){
+    str += "-";
+    num *= (-1);
+  }
+  while(num > 0){
+    check = num % 10;
+      switch(check){
+          case 0:
+            str1+="0";
+            break;
+          case 1:
+            str1+="1";
+            break;
+          case 2:
+            str1+="2";
+            break;
+          case 3:
+            str1+="3";
+            break;
+          case 4:
+            str1+="4";
+            break;
+          case 5:
+            str1+="5";
+          break;
+          case 6:
+            str1+="6";
+            break;
+          case 7:
+            str1+="7";
+            break;
+          case 8:
+            str1+="8";
+             break;
+           case 9:
+            str1+="9";
+             break;
+            default: break;}
+     num /= 10;
+}
+  
+  for(int i = str1.length()-1; i>=0; --i){
+    str += str1[i];
+  }
+  return str;
+}
+
+
+#include <string>             //no_space
+
+std::string no_space(const std::string& x){
+  std::string str = "";
+  for(int i = 0; i < x.length(); ++i){
+    if(x[i] != ' '){
+      str += x[i];
+    }
+  }
+  return str;
+}
+
+
+
+
